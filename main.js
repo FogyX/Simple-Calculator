ac8ff341cf6a39b68238e49bdc6b100d5c415fd8
FogyX <maksdima0515@gmail.com>
2022-03-26 12:55:24 +0300

    Initial project version

---
+++ main.js
@@ -0,0 +1,100 @@
+/**@type {HTMLInputElement} */
+let inputField = document.getElementById('inp');
+
+/**@type {HTMLElement} */
+let outputField = document.getElementById('out');
+
+const buttons = document.querySelectorAll('.ui_button');
+
+const validSymbols = ['+', '-', '*', '/'];
+
+const options = {
+    'button7': buttonAddSymbol,
+    'button8': buttonAddSymbol,
+    'button9': buttonAddSymbol,
+    'buttonPLus': buttonAddSymbol,
+    'button4': buttonAddSymbol,
+    'button5': buttonAddSymbol,
+    'button6': buttonAddSymbol,
+    'buttonMinus': buttonAddSymbol,
+    'button1': buttonAddSymbol,
+    'button2': buttonAddSymbol,
+    'button3': buttonAddSymbol,
+    'buttonMultiply': buttonAddSymbol,
+    'buttonDelete': buttonDelete,
+    'button0': buttonAddSymbol,
+    'buttonEquals': buttonEquals,
+    'buttonDivide': buttonAddSymbol,
+    'buttonClear': buttonClear
+}
+
+buttons.forEach(element => {
+    element.addEventListener('click', onButtonDown)
+});
+
+/**
+ * @param {Event} event
+ */
+function onButtonDown(event) {
+
+    /** @type {Function} */
+    let funcToExec = options[event.currentTarget.id];
+
+    let symbol = event.currentTarget.firstChild.textContent;
+
+    funcToExec(symbol);
+    checkLength();
+
+}
+
+function checkLength() {
+    if (inputField.value.length > inputField.maxLength) {
+        inputField.value =  inputField.value.slice(0, -1);
+    }
+}
+/*
+* Вот тут идут все функции кнопок
+*/
+
+/**
+ * 
+ * @param {String} symbol 
+ */
+function buttonAddSymbol(symbol) {
+    inputField.value += symbol;
+}
+
+function buttonDelete() {
+    inputField.value =  inputField.value.slice(0, -1);
+}
+
+function buttonClear() {
+    inputField.value = '';
+}
+
+function buttonEquals() {
+    let isValid = true;
+    let value = inputField.value;
+
+    for (let i = 0; i < value.length; i++) {
+        if (isNaN(value[i]) && !(validSymbols.includes(value[i]))) {
+
+            isValid = false;
+            break;
+        }
+    }
+
+    if (!isValid) {
+        outputField.textContent = 'Invalid input!';
+        inputField.value = '';
+        return;
+    }
+    try {
+        outputField.textContent = eval(value);
+        inputField.value = '';
+    } catch (error) {
+        outputField.textContent = 'Invalid input!'
+        inputField.value = '';
+    }
+    
+}
